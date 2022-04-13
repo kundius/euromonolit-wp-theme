34b9398c8a0f8d7bed938d8c088c626fb78a3a6e
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-13 17:54:22 +0300

    upd

--- inc/Services/Seo.php
+++ inc/Services/Seo.php
@@ -26,7 +26,6 @@ class Seo implements Service
     public function boot(Service_Container $container): void
     {
         \remove_action('wp_head', 'rel_canonical');
-        
         \add_action('wp_head', [$this, 'add_canonical']);
         \add_action('wp_head', [$this, 'add_meta']);
     }
