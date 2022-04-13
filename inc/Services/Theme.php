19b1012853296460c01c3ddfa65b196b24cdc13c
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-13 18:40:09 +0300

    u

--- inc/Services/Theme.php
+++ inc/Services/Theme.php
@@ -60,12 +60,13 @@ class Theme implements Service
         // });
 
         // Add the theme support basic elements
-        \add_theme_support('align-wide');
-        \add_theme_support('responsive-embeds');
-        \add_theme_support('editor-styles');
-        \add_theme_support('wp-block-styles');
+        // \add_theme_support('align-wide');
+        // \add_theme_support('responsive-embeds');
+        // \add_theme_support('editor-styles');
+        // \add_theme_support('wp-block-styles');
         \add_theme_support('post-thumbnails');
-        \add_theme_support('html5', ['comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'script', 'style']);
+        // \add_theme_support('html5', ['comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'script', 'style']);
+
         // \add_theme_support('title-tag');
         // \add_theme_support('async-js');
         // \add_theme_support('yoast-seo-breadcrumbs');
