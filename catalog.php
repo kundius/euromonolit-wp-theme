b5d676d8dacc46e52aecb78287b4b43ac7730f8c
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-21 15:46:31 +0300

    upd

--- catalog.php
+++ catalog.php
@@ -1,13 +1,13 @@
 <?php
 /*
 Template Name: Каталог
-*/
+ */
 
 $products = new WP_Query([
-  'post_type' => 'page',
-  'post_parent' => get_the_ID(),
-  'order' => 'ASC',
-  'orderby' => 'menu_order'
+    'post_type' => 'page',
+    'post_parent' => get_the_ID(),
+    'order' => 'ASC',
+    'orderby' => 'menu_order',
 ]);
 ?>
 <!DOCTYPE html>
@@ -19,20 +19,20 @@ $products = new WP_Query([
     <?php wp_body_open();?>
 
     <div class="ui-wrapper">
-      <?php get_template_part('partials/header') ?>
+      <?php get_template_part('partials/header')?>
 
       <div class="page-breadcrumbs">
         <div class="ui-container">
-          <?php bcn_display(true) ?>
+          <?php bcn_display(true)?>
         </div>
       </div>
 
       <div class="page-headline">
         <div class="ui-container">
-          <h1 class="page-headline__title"><?php the_title() ?></h1>
+          <h1 class="page-headline__title"><?php the_title()?></h1>
           <?php if ($description = get_field('page_description')): ?>
           <div class="page-headline__description"><?php echo $description ?></div>
-          <?php endif ?>
+          <?php endif?>
         </div>
       </div>
 
@@ -43,7 +43,7 @@ $products = new WP_Query([
           <div class="catalog-body__intro-text">
             <?php echo $intro_text ?>
           </div>
-          <?php endif ?>
+          <?php endif?>
 
           <?php if ($special_offer = get_field('special-offer') && $special_offer['show']): ?>
           <div class="catalog-body__special-offer">
@@ -60,11 +60,11 @@ $products = new WP_Query([
               <button class="special-offer-sm__close"></button>
             </div>
           </div>
-          <?php endif ?>
+          <?php endif?>
 
           <?php if ($products->have_posts()): ?>
           <div class="catalog__grid">
-            <?php foreach($products as $product): ?>
+            <?php foreach ($products as $product): ?>
             <div class="catalog__grid-cell">
               <article class="catalog-card">
                 <figure class="catalog-card__image">
@@ -72,26 +72,26 @@ $products = new WP_Query([
                 </figure>
                 <div class="catalog-card__headline">
                   <h2 class="catalog-card__title">
-                    <?php the_title($product->ID) ?>
+                    <?php the_title($product->ID)?>
                   </h2>
                   <div class="catalog-card__subtitle">
-                    <?php the_field('product_price', $product->ID) ?>
+                    <?php the_field('product_price', $product->ID)?>
                   </div>
                 </div>
                 <div class="catalog-card__description">
-                    <?php the_field('product_description') ?>
+                    <?php the_field('product_description')?>
                 </div>
                 <div class="catalog-card__more">
-                  <a href="<?php the_permalink($product->ID) ?>" class="ui-button-more" data-hystmodal="#feedback">
+                  <a href="<?php the_permalink($product->ID)?>" class="ui-button-more" data-hystmodal="#feedback">
                     Узнать больше
                     <span class="ui-arrow-right"></span>
                   </a>
                 </div>
               </article>
             </div>
-            <?php endwhile; ?>
+            <?php endforeach;?>
           </div>
-          <?php endif ?>
+          <?php endif?>
 
           <?php if ($emulsifier = get_field('emulsifier') && $emulsifier['show']): ?>
           <div class="catalog-body__emulsifier">
@@ -104,22 +104,22 @@ $products = new WP_Query([
               </div>
             </div>
           </div>
-          <?php endif ?>
+          <?php endif?>
 
           <div class="content">
-            <?php the_content() ?>
+            <?php the_content()?>
           </div>
 
         </div>
       </div>
 
-      <?php get_template_part('partials/section-formwork-advantages') ?>
+      <?php get_template_part('partials/section-formwork-advantages')?>
 
-      <?php get_template_part('partials/section-how-we-work') ?>
+      <?php get_template_part('partials/section-how-we-work')?>
 
-      <?php get_template_part('partials/section-get-estimate') ?>
+      <?php get_template_part('partials/section-get-estimate')?>
 
-      <?php get_template_part('partials/footer') ?>
+      <?php get_template_part('partials/footer')?>
     </div>
   </body>
 </html>
