d1223a4b7f27ad9c2494dfe0dc8975887a527043
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-21 19:20:51 +0300

    deploy

--- partials/side-news.php
+++ partials/side-news.php
@@ -12,27 +12,23 @@ $articles = new WP_Query([
   
   <?php if ($articles->have_posts()): ?>
   <div class="side-news__articles">
-    <div class="side-news-grid">
-      <?php while ($articles->have_posts()): ?>
-      <?php $articles->the_post()?>
-      <div class="side-news-grid__cell">
-        <article class="side-news-card">
-          <figure class="side-news-card__image">
-            <img src="<?php the_post_thumbnail_url('thumbnail')?>" alt="<?php the_title()?>" />
-          </figure>
-          <div class="side-news-card__body">
-            <div class="blog-card__date">
-              <?php the_date('d.m.Y')?>
-            </div>
-            <div class="blog-card__title">
-              <a href="<?php the_permalink()?>"><?php the_title()?></a>
-            </div>
-          </div>
-        </article>
+    <?php while ($articles->have_posts()): ?>
+    <?php $articles->the_post()?>
+    <article class="side-news-card">
+      <figure class="side-news-card__image">
+        <img src="<?php the_post_thumbnail_url('thumbnail')?>" alt="<?php the_title()?>" />
+      </figure>
+      <div class="side-news-card__body">
+        <div class="side-news-grid__date">
+          <?php the_date('d.m.Y')?>
+        </div>
+        <div class="side-news-grid__title">
+          <a href="<?php the_permalink()?>"><?php the_title()?></a>
+        </div>
       </div>
-      <?php endwhile?>
-      <?php wp_reset_postdata()?>
-    </div>
+    </article>
+    <?php endwhile?>
+    <?php wp_reset_postdata()?>
   </div>
   <?php endif?>
 </div>
