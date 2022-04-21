61bd801ee6e3efffdcd335ce915deb8aceaa2fd7
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-21 13:55:23 +0300

    deploy

--- partials/section-formwork-advantages.php
+++ partials/section-formwork-advantages.php
@@ -1,74 +1,37 @@
+<?php $advantages = get_field('advantages', 'options') ?>
 <section class="formwork-advantages">
   <div class="ui-container">
     <div class="formwork-advantages__headline">
-      <div class="formwork-advantages__headline-first">Преимущества</div>
-      <div class="formwork-advantages__headline-seacond">аренда опалубка</div>
-      <div class="formwork-advantages__headline-third">в коттеджном.загородном строительстве</div>
+      <?php if ($advantages['title']['first']): ?>
+      <div class="formwork-advantages__headline-first"><?php echo $advantages['title']['first'] ?></div>
+      <?php endif ?>
+      <?php if ($advantages['title']['second']): ?>
+      <div class="formwork-advantages__headline-second"><?php echo $advantages['title']['second'] ?></div>
+      <?php endif ?>
+      <?php if ($advantages['title']['third']): ?>
+      <div class="formwork-advantages__headline-third"><?php echo $advantages['title']['third'] ?></div>
+      <?php endif ?>
     </div>
 
     <div class="formwork-advantages__items">
       <div class="formwork-advantages__grid">
+        <?php foreach ($advantages['items'] as $item): ?>
         <div class="formwork-advantages__grid-cell">
           <div class="formwork-advantages-item">
             <div class="formwork-advantages-item__icon">
-              <img src="<?php bloginfo('template_url') ?>/dist/images/formwork-advantages-1.svg" alt="" />
+              <img src="<?php echo $item['image']['url'] ?>" alt="" />
             </div>
             <div class="formwork-advantages-item__body">
               <div class="formwork-advantages-item__title">
-                Экономия средств бюджета
+                <?php echo $item['title'] ?>
               </div>
               <div class="formwork-advantages-item__title">
-                За счет сокращения сроков строительства и гибких сроков аренды. Выгоднее опалубок несъемных и опалубливания досками
+                <?php echo $item['description'] ?>
               </div>
             </div>
           </div>
         </div>
-        <div class="formwork-advantages__grid-cell">
-          <div class="formwork-advantages-item">
-            <div class="formwork-advantages-item__icon">
-              <img src="<?php bloginfo('template_url') ?>/dist/images/formwork-advantages-2.svg" alt="" />
-            </div>
-            <div class="formwork-advantages-item__body">
-              <div class="formwork-advantages-item__title">
-                Экономия места на стройплощадке
-              </div>
-              <div class="formwork-advantages-item__title">
-                Вам не нужно будет думать, где хранить оборудование, когда оно не используется
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="formwork-advantages__grid-cell">
-          <div class="formwork-advantages-item">
-            <div class="formwork-advantages-item__icon">
-              <img src="<?php bloginfo('template_url') ?>/dist/images/formwork-advantages-3.svg" alt="" />
-            </div>
-            <div class="formwork-advantages-item__body">
-              <div class="formwork-advantages-item__title">
-                Сокращение сроков строительства
-              </div>
-              <div class="formwork-advantages-item__title">
-                Быстрый расчет и доставка на объект;<br />
-                Быстрая сборка/разборка.
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="formwork-advantages__grid-cell">
-          <div class="formwork-advantages-item">
-            <div class="formwork-advantages-item__icon">
-              <img src="<?php bloginfo('template_url') ?>/dist/images/formwork-advantages-4.svg" alt="" />
-            </div>
-            <div class="formwork-advantages-item__body">
-              <div class="formwork-advantages-item__title">
-                Гибкость в подборе видов опалубки
-              </div>
-              <div class="formwork-advantages-item__title">
-                Вы можете подобрать оборудование для строительства именно вашего объекта
-              </div>
-            </div>
-          </div>
-        </div>
+        <?php endforeach ?>
       </div>
     </div>
   </div>
