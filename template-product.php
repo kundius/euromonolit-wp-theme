724a05d0c5daba49afed352d1e9408b857500b0f
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-25 20:32:22 +0300

    deploy

--- template-product.php
+++ template-product.php
@@ -3,6 +3,13 @@
 Template Name: Продукт
 */
 $title = get_the_title();
+$title_size = 'noramal';
+if (strlen($title > 50)) {
+  $title_size = 'medium';
+}
+if (strlen($title > 100)) {
+  $title_size = 'small';
+}
 ?>
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes();?> itemscope itemtype="http://schema.org/WebSite">
@@ -23,7 +30,7 @@ $title = get_the_title();
 
       <div class="page-headline">
         <div class="ui-container">
-          <h1 class="page-headline__title"><?php echo $title ?> <?php echo strlen($title) ?></h1>
+          <h1 class="page-headline__title <?php echo 'page-headline__title_' . $title_size ?>"><?php echo $title ?></h1>
           <?php if ($description = get_field('page_description')): ?>
           <div class="page-headline__description"><?php echo $description ?></div>
           <?php endif ?>
