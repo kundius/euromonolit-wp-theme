adcea39185bd1eee2454e17175d7e2d0233b12eb
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-27 00:18:24 +0300

    deploy

--- template-album.php
+++ template-album.php
@@ -2,6 +2,8 @@
 /*
 Template Name: Альбом
  */
+$per_page = 4;
+$current_page = get_query_var('paged') ? get_query_var('paged') : 1;
 $gallery = get_field('album_gallery');
 ?>
 <!DOCTYPE html>
@@ -17,8 +19,18 @@ $gallery = get_field('album_gallery');
       <?php get_template_part('partials/page-breadcrumbs') ?>
       <?php get_template_part('partials/page-headline') ?>
 
+        <?php print_r($current_page) ?>
       <div class="album">
         <?php print_r($gallery) ?>
+      </div>
+
+      <div class="album-pagination">
+        <?php the_posts_pagination([
+          'prev_text' => '',
+          'next_text' => '',
+          'total' => 1,
+          'current' => 0,
+        ]) ?>
       </div>
 
       <div class="page-body album-body">
