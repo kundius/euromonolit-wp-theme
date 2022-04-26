bf71b4cec4a96cbc14728611ce6e4f3e9c18a6d4
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-26 20:17:26 +0300

    deploy

--- template-contacts.php
+++ template-contacts.php
@@ -74,14 +74,14 @@ Template Name: Контакты
                 <input class="contacts-map__radio" id="one" name="group" type="radio" checked>
                 <input class="contacts-map__radio" id="two" name="group" type="radio">
                 <div class="contacts-map__tabs">
-                  <label class="contacts-map__tab" id="one-tab" for="one">Офис на карте</label>
-                  <label class="contacts-map__tab" id="two-tab" for="two">Склад на карте</label>
+                  <label class="contacts-map__tab" for="one">Офис на карте</label>
+                  <label class="contacts-map__tab" for="two">Склад на карте</label>
                 </div>
                 <div class="contacts-map__panels">
-                  <div class="contacts-map__panel" id="one-panel">
+                  <div class="contacts-map__panel">
                     <p>Without CSS, every web page would be drab plain text and images that flowed straight down the page. With CSS, you can add color and background images and change the layout of your page — your web pages can feel like works of art!</p>
                   </div>
-                  <div class="contacts-map__panel" id="two-panel">
+                  <div class="contacts-map__panel">
                     <p>You will learn many aspects of styling web pages! You’ll be able to set up the correct file structure, edit text and colors, and create attractive layouts. With these skills, you’ll be able to customize the appearance of your web pages to suit your every need!</p>
                   </div>
                 </div>
