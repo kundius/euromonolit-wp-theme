bbc9b8e2dda014f6668d42d491e8187050c72772
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-06-01 19:51:29 +0000

    виды опалубки вернули

--- landing.php
+++ landing.php
@@ -188,38 +188,6 @@ $products = new WP_Query([
               </div>
               <?php endwhile?>
               <?php wp_reset_postdata()?>
-
-              <?php if ($components = get_post(150)): ?>
-              <div class="landing-catalog__grid-cell">
-                <article class="catalog-card">
-                  <figure class="catalog-card__image">
-                    <img src="<?php echo get_the_post_thumbnail_url($components, 'theme-medium')?>" alt="<?php echo get_the_title($components)?>" />
-                    <button class="catalog-card__order" data-hystmodal="#feedback">
-                      <span>Отправить заявку на аренду</span>
-                    </button>
-                  </figure>
-                  <div class="catalog-card__body">
-                    <div class="catalog-card__headline">
-                      <h2 class="catalog-card__title">
-                        <?php echo get_the_title($components)?>
-                      </h2>
-                      <div class="catalog-card__subtitle">
-                        <?php the_field('product_price', $components)?>
-                      </div>
-                    </div>
-                    <div class="catalog-card__description">
-                        <?php the_field('product_description', $components)?>
-                    </div>
-                    <div class="catalog-card__more">
-                      <a href="<?php the_permalink($components)?>" class="ui-button-more" data-hystmodal="#feedback">
-                        Узнать больше
-                        <span class="ui-arrow-right"></span>
-                      </a>
-                    </div>
-                  </div>
-                </article>
-              </div>
-              <?php endif?>
             </div>
           </div>
 
